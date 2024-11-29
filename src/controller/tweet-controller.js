1d0e143a646b0afd8eb2af86f3d520e8689e73c7
KUSHANK PATIL <patilkushank@gmail.com>
2024-11-29 10:46:50 +0530

    Added Controllers and Routes for creating Tweets

---
+++ src/controller/tweet-controller.js
@@ -0,0 +1,21 @@
+import TweetService from '../services/tweet-service.js';
+const service = new TweetService();
+export const create = async (req,res)=>{
+    try {
+        const response = await service.create(req.body);
+            res.status(201).json({
+             success:true,
+             message:"Succesfully Created Tweet",
+             data:response,
+             err:{}
+
+            });
+        } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Something Went wrong while Creating Tweet",
+            data:{},
+            err:error
+           })
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import {connect} from './config/database.js';
 import HashtagRepository from './repository/hashtag-repository.js';
 import TweetService from './services/tweet-service.js';
+import apiRoutes from "./routes/index.js"
 
 const app = express();
 
 app.listen(3000,async()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     console.log('Server Is Running');
     await connect();
     console.log('MongoDb connected')
     let service= new TweetService();
-    let tweet =  await service.create({
-        content:"I am Really #excited to begin this #new #journey with #netflix as a #software #engineer"
-    })
-   console.log(tweet);
+    app.use('/api',apiRoutes);
 })
