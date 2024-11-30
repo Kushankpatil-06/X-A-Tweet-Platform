3c60db5f1e8e107f2b4651ecc5b74a724b169d4d
KUSHANK PATIL <patilkushank@gmail.com>
2024-11-30 11:31:02 +0530

    Added SignUp Api with Password Encryption

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-// import bcrypt from 'bcrypt'; 
+import bcrypt from 'bcrypt'; 
 // import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
@@ -17,6 +17,14 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 }, {timestamps: true});
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const encryptedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encryptedPassword;
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 export default User;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {create,getTweets} from "../../controller/tweet-controller.js"
 import { toggleLike } from '../../controller/like-controller.js';
 import { createComment,  } from '../../controller//comment-controller.js'
+import { signup } from '../../controller/auth-controller.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.post('/tweets',create)
 router.post('/toggle/like',toggleLike)
 router.post('/comments', createComment);
 router.get('/tweets/:id',getTweets );
+router.post('/signUp',signup)
 
 export default router;
