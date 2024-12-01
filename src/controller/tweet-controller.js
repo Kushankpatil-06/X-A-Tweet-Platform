85029672a899149856b522d099fb9e6f649e81df
KUSHANK PATIL <patilkushank@gmail.com>
2024-12-01 15:44:16 +0530

    Added Image Upload in Tweet creation

--- src/controller/tweet-controller.js
+++ src/controller/tweet-controller.js
@@ -1,22 +1,35 @@
-import TweetService from '../services/tweet-service.js';
-const service = new TweetService();
-export const create = async (req,res)=>{
-    try {
-        const response = await service.create(req.body);
-            res.status(201).json({
-            success:true,
-            message:"Succesfully Created Tweet",
-            data:response,
-            err:{}
+import TweetService from "../services/tweet-service.js";
 
+import upload from '../config/file-upload-config.js';
+
+const singleUploader = upload.single('image');
+
+const tweetService = new TweetService();
+
+export const createTweet = async (req, res) => {
+    try {
+        singleUploader(req, res, async function (err, data) {
+            if(err) {
+                return res.status(500).json({error: err});
+            }
+            console.log('Image url is', req.file);
+            const payload = {...req.body};
+            payload.image = req.file.location;
+            const response = await tweetService.create(payload);
+            return res.status(201).json({
+                success: true,
+                message: 'Successfully created a new tweet',
+                data: response,
+                err: {}
             });
-        } catch (error) {
+        });
+    } catch (error) {
         return res.status(500).json({
-            success:false,
-            message:"Something Went wrong while Creating Tweet",
-            data:{},
-            err:error
-           })
+            success: false,
+            message: 'something went wrong',
+            data: {},
+            err: error
+        });
     }
 }
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {create,getTweets} from "../../controller/tweet-controller.js"
+import {createTweet,getTweets} from "../../controller/tweet-controller.js"
 import { toggleLike } from '../../controller/like-controller.js';
 import { createComment,  } from '../../controller//comment-controller.js'
 import { signup,login } from '../../controller/auth-controller.js';
@@ -8,7 +8,7 @@ import { authenticate } from '../../config/authenticate.js';
 const router = express.Router();
 
 
-router.post('/tweets',authenticate,create)
+router.post('/tweets',authenticate,createTweet)
 router.post('/toggle/like',authenticate,toggleLike)
 router.post('/comments', authenticate,createComment);
 router.get('/tweets/:id',getTweets );
